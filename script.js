53dfa88594129b3a1365fd7f37d7b85073325d30
di <di@gmail.com>
2024-03-15 00:53:52 +0700

    update slider

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+var button = document.getElementById("top-btn");
+
+window.onscroll = function() {
+if (window.scrollY > 200) {
+button.classList.add("top-btn-is-visible");
+} else {
+button.classList.remove("top-btn-is-visible");
+}
+};
+
+button.addEventListener("click", function(event) {
+event.preventDefault();
+window.scrollTo({top: 0, behavior: "smooth"});
+});y
